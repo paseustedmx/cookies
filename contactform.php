e9b2149ff66c2ff6066125ac9704fb4ffd28a2e7
Mauricio Reyes <hola@paseusted.com.mx>
2021-08-20 17:19:54 -0500

    web inicial

---
+++ contactform.php
@@ -0,0 +1,21 @@
+<?php 
+
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $message = $_POST['message'];
+    $submit = $_POST['submit'];
+
+    $mailTo = "soporte@paseusted.com.mx";
+    $headers = "De: ".$email ;
+    $txt = "hemos recibido un email de: ".$name.".\n\n";
+    $phone = "Mi numero es: ".$number. "y mi correo es: ".$email;
+    $mensaje = "mi mensaje es: ".$message;
+    mail($mailTo,$txt, $phone, $mensaje);
+    
+}
+header("Location: index.html?mailsend");
+
+
+?>
